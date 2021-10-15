dd7e4ffe29ab5611852ff51ee509cf6b384f3723
Kevincalderon17 <89594166+Kevincalderon17@users.noreply.github.com>
2021-10-15 00:10:06 -0500

    Add files via upload

---
+++ Programa 001/Funciones.js
@@ -0,0 +1,99 @@
+document.write("NÚMEROS BINARIOS<br>");
+document.write("-------------------------------<br><br>");
+document.write("Número: 0000 = ");
+
+var x;
+
+x = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 0001 = ")
+
+x = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 0010 = ")
+
+x = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 0011 = ")
+
+x = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 0100 = ")
+
+x = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 0101 = ")
+
+x = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 0110 = ")
+
+x = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 0111 = ")
+
+x = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1000 = ")
+
+x = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1001 = ")
+
+x = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1010 = ")
+
+x = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1011 = ")
+
+x = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1100 = ")
+
+x = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1101 = ")
+
+x = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1110 = ")
+
+x = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write(x + "<br>");
+
+document.write("Número: 1111 = ")
+
+x = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write(x + "<br>")
